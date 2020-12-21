87ddabae15bc5e41e245dc744158a65d18887671
田林宝 <2158663045@qq.com>
2020-12-21 19:06:31 +0800

    田林宝提交

--- jdoa/src/main/java/com/study/controller/testcontroller.java
+++ jdoa/src/main/java/com/study/controller/testcontroller.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testcontroller {
     @RequestMapping("a")
     public String test(){
-        return "111";
+        return "22";
     }
 }
