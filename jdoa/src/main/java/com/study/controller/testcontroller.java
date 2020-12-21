ba92e20715c5a506efd97829c670cc05462811ac
tlb456 <76423985+tlb456@users.noreply.github.com>
2020-12-21 19:17:10 +0800

    Update testcontroller.java

--- jdoa/src/main/java/com/study/controller/testcontroller.java
+++ jdoa/src/main/java/com/study/controller/testcontroller.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testcontroller {
     @RequestMapping("a")
     public String test(){
-        return "22";
+        return "222";
     }
 }
