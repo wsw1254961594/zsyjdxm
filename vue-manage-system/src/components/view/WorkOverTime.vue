9259d4364b537f7c5da020046f3b586399e48514
baixuanzi <3163059772@qq.com>
2020-12-22 18:59:01 +0800

    登录完善，请假流程申请

---
+++ vue-manage-system/src/components/view/WorkOverTime.vue
@@ -0,0 +1,20 @@
+<!--
+@author: pengjia
+@date: 2020/12/22
+@description:
+-->
+<template>
+
+</template>
+
+<script>
+
+  export default {
+    name: "WorkOverTime",
+
+  }
+</script>
+
+<style scoped>
+
+</style>
